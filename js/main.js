9216e5611849b9d3c63f961db83568c5a576fcca
Arnav Nagpurkar <91673831+linux-07@users.noreply.github.com>
2024-03-27 20:42:29 +0530

    Slider not working fixed

--- js/main.js
+++ js/main.js
@@ -1,41 +1,17 @@
-let menu = document.querySelector('.fa-bars');
-let navbar = document.querySelector('.navbar');
-
-menu.addEventListener('click', function () {
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('nav-toggle');
+document.addEventListener("DOMContentLoaded", function () {
+  changeText(); // Initialize text change
+  showSlides(slideIndex); // Initialize slider
 });
-
-window.addEventListener('scroll', () => {
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('nav-toggle');
-});
-
-
-
-const texts = ["Save Water, Save Life", "Clean Water, Live Healthy", "Save Blue, Live Green", "Think Before You Let It Drip", "Every Drop Counts"];
-function changeText() {
-  const sloganElement = document.getElementById("slogan");
-  let currentIndex = 0;
-
-  setInterval(function () {
-    sloganElement.textContent = texts[currentIndex];
-    currentIndex = (currentIndex + 1) % texts.length;
-  }, 4000);
-}
-changeText();
-
 
 let slideIndex = 1;
-showSlides(slideIndex);
 
 const plusSlides = (n) => {
-  showSlides(slideIndex += n);
-}
+  showSlides((slideIndex += n));
+};
 
 const currentSlide = (n) => {
-  showSlides(slideIndex = n);
-}
+  showSlides((slideIndex = n));
+};
 
 const showSlides = (n) => {
   let i;
@@ -60,4 +36,35 @@ const showSlides = (n) => {
 
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
+};
+
+let menu = document.querySelector(".fa-bars");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", function () {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("nav-toggle");
+});
+
+window.addEventListener("scroll", () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("nav-toggle");
+});
+
+const texts = [
+  "Save Water, Save Life",
+  "Clean Water, Live Healthy",
+  "Save Blue, Live Green",
+  "Think Before You Let It Drip",
+  "Every Drop Counts",
+];
+function changeText() {
+  const sloganElement = document.getElementById("slogan");
+  let currentIndex = 0;
+
+  setInterval(function () {
+    sloganElement.textContent = texts[currentIndex];
+    currentIndex = (currentIndex + 1) % texts.length;
+  }, 4000);
 }
+changeText();
